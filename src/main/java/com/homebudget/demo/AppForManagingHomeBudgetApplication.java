c196b3f5c057e5303e24e17280a4f3c1b765db1f
Cezary Chojnowski <chojnowski.czarek@gmail.com>
2019-07-15 11:08:03 +0200

    Create project

---
+++ src/main/java/com/homebudget/demo/AppForManagingHomeBudgetApplication.java
@@ -0,0 +1,13 @@
+package com.homebudget.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppForManagingHomeBudgetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppForManagingHomeBudgetApplication.class, args);
+    }
+
+}
