f2a6d0217fbf19aaa2df9bec286cc1da5e67307a
Cezary Chojnowski <chojnowski.czarek@gmail.com>
2019-07-16 12:58:07 +0200

    Add spring security dependency to pom.xml Create Bean BCryptPasswordEncoder Injecting dependencies bCryptPasswordEncoder by the constructor Disable default security Creating the createUser method which adds a new user to the database Password encrypted by uses the BCrypt strong hashing function

--- src/main/java/com/dao/UserDAO.java
+++ src/main/java/com/dao/UserDAO.java
@@ -4,6 +4,7 @@ import com.entity.User;
 import com.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import java.util.List;
 
@@ -12,9 +13,12 @@ public class UserDAO {
 
     private UserRepository userRepository;
 
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
     @Autowired
-    public UserDAO(UserRepository theUserRepository){
+    public UserDAO(UserRepository theUserRepository, BCryptPasswordEncoder ThebCryptPasswordEncoder){
         userRepository = theUserRepository;
+        bCryptPasswordEncoder = ThebCryptPasswordEncoder;
     }
 
     public List<User> findAllUsers(){
@@ -27,5 +31,13 @@ public class UserDAO {
 
     public boolean checkTheUniqueEmail(String email){
         return findUserByEmail(email) == null;
+    }
+
+    public User createUser(String login, String password, String email){
+        User user = new User();
+        user.setLogin(login);
+        user.setPassword(bCryptPasswordEncoder.encode(password));
+        user.setEmail(email);
+        return userRepository.save(user);
     }
 }
