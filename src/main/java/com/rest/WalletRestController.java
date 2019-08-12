97bb70b88980f90284c287f356063f67844e9bf6
Cezary Chojnowski <chojnowski.czarek@gmail.com>
2019-08-12 10:24:05 +0200

    A request for the createNewWallet method add the wallet. If the user has wallet with given name, the method returns error

--- src/main/java/com/rest/WalletRestController.java
+++ src/main/java/com/rest/WalletRestController.java
@@ -1,10 +1,17 @@
 package com.rest;
 
+import com.dao.UserDAO;
 import com.dao.WalletDAO;
+import com.entity.User;
 import com.entity.Wallet;
+import com.error.ErrorResponse;
+import com.error.Exception;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
@@ -13,9 +20,12 @@ public class WalletRestController {
 
     private WalletDAO walletDAO;
 
+    private UserDAO userDAO;
+
     @Autowired
-    public WalletRestController(WalletDAO theWalletDAO){
+    public WalletRestController(WalletDAO theWalletDAO, UserDAO theUserDAO){
         walletDAO = theWalletDAO;
+        userDAO = theUserDAO;
     }
 
     @GetMapping("/getAllWallets")
@@ -26,5 +36,26 @@ public class WalletRestController {
     @GetMapping("/myWallets")
     public List<Wallet> getMyWallets(@RequestParam Integer userID){
         return walletDAO.findUserWallets(userID);
+    }
+
+    @PostMapping("/createNewWallet")
+    public ResponseEntity createNewWallet(@RequestParam Integer userID,
+                                  @Valid @RequestBody Wallet wallet){
+        User user = userDAO.findUserByID(userID);
+        if(walletDAO.checkIfUserHasWalletWithTheGivenName(user.getWallets(), wallet.getName_wallet())){
+            throw new Exception("User has a wallet with this name " + wallet.getName_wallet());
+        }
+        else{
+            return ResponseEntity.ok(walletDAO.createNewWallet(wallet, userID));
+        }
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<ErrorResponse> handleException(Exception existEx){
+        ErrorResponse error = new ErrorResponse();
+        error.setStatus(HttpStatus.INSUFFICIENT_STORAGE.value());
+        error.setMessage(existEx.getMessage());
+        error.setTimeStamp(System.currentTimeMillis());
+        return new ResponseEntity<>(error, HttpStatus.INSUFFICIENT_STORAGE);
     }
 }
