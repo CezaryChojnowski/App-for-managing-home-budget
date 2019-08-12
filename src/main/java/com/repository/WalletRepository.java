0aeac5e165a21ad90b7ec8b9813049366ae387db
Cezary Chojnowski <chojnowski.czarek@gmail.com>
2019-08-12 09:11:56 +0200

    Method to find all user wallets with a given ID

--- src/main/java/com/repository/WalletRepository.java
+++ src/main/java/com/repository/WalletRepository.java
@@ -2,6 +2,13 @@ package com.repository;
 
 import com.entity.Wallet;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+@Repository
 public interface WalletRepository extends JpaRepository<Wallet, Integer> {
+
+    @Query(value = "SELECT w FROM Wallet w, User u WHERE w.user=u and u.id = ?1")
+    List<Wallet> findUserWallets(Integer id);
 }
