fef80ad20f56943b24377e30c56b668643e94a52
Cezary Chojnowski <chojnowski.czarek@gmail.com>
2019-08-12 09:26:28 +0200

    Implementation method to find all user wallets

--- src/main/java/com/dao/WalletDAO.java
+++ src/main/java/com/dao/WalletDAO.java
@@ -1,7 +1,9 @@
 package com.dao;
 
 import com.entity.Wallet;
+import com.repository.UserRepository;
 import com.repository.WalletRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -11,11 +13,20 @@ public class WalletDAO {
 
     private WalletRepository walletRepository;
 
-    public WalletDAO(WalletRepository theWalletRepository){
+    private UserRepository userRepository;
+
+    @Autowired
+    public WalletDAO(WalletRepository theWalletRepository, UserRepository theUserRepository){
         walletRepository = theWalletRepository;
+        userRepository = theUserRepository;
     }
 
     public List<Wallet> findAllWallet(){
         return walletRepository.findAll();
     }
+
+    public List<Wallet> findUserWallets(Integer id){
+        return walletRepository.findUserWallets(id);
+    }
+
 }
