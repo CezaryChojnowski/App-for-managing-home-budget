5ddd2b2b92859f66116a105ca9889d6c393f6703
Cezary Chojnowski <chojnowski.czarek@gmail.com>
2019-08-12 09:32:31 +0200

    Not important edit

--- src/main/java/com/error/UserExistsException.java
+++ src/main/java/com/error/UserExistsException.java
@@ -5,12 +5,4 @@ public class UserExistsException extends RuntimeException{
     public UserExistsException(String message) {
         super(message);
     }
-
-    public UserExistsException(String message, Throwable cause) {
-        super(message, cause);
-    }
-
-    public UserExistsException(Throwable cause) {
-        super(cause);
-    }
 }
