6f896c889f3a3107ee8870b7cf0e00caf0d95b81
Cezary Chojnowski <chojnowski.czarek@gmail.com>
2019-09-10 17:01:40 +0200

    Changing packages

--- src/main/java/com/User/UserRepository.java
+++ src/main/java/com/User/UserRepository.java
@@ -1,19 +1,13 @@
-package com.repository;
+package com.User;
 
-import com.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
-//it is a good practise to return optional if resource could not exists
-
-//do not use @Query - read about how HQL works
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
 
     Optional<User> findUserByEmail(String email);
 
-    Optional<User> findUserById(Integer id);
 }
