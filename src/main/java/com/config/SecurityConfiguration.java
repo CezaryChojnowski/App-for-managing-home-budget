6ee2d5a74b6a5f901af5f5d50660f43706d37ac8
Cezary Chojnowski <chojnowski.czarek@gmail.com>
2019-09-10 17:04:42 +0200

    Creating the UserDetailsService class that will allow user authentication to be checked during method execution

--- src/main/java/com/config/SecurityConfiguration.java
+++ src/main/java/com/config/SecurityConfiguration.java
@@ -1,23 +1,40 @@
 package com.config;
 
-import org.springframework.context.annotation.Bean;
+import com.service.UserDetailsService;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
+@EnableConfigurationProperties
+@EnableGlobalMethodSecurity(
+        prePostEnabled = true,
+        securedEnabled = true,
+        jsr250Enabled = true)
+@AllArgsConstructor
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
-    @Bean
-    public BCryptPasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
+    private UserDetailsService userDetailsService;
+
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
 
     @Override
-    protected void configure(HttpSecurity security) throws Exception
-    {
-
-        security.httpBasic().disable().csrf().disable();
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable().authorizeRequests()
+                .antMatchers("/users").permitAll()
+                .anyRequest().authenticated()
+                .and().httpBasic();
+    }
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder builder) throws Exception {
+        builder
+                .userDetailsService(userDetailsService)
+                .passwordEncoder(bCryptPasswordEncoder);
     }
 }
---
+++ src/main/java/com/service/UserDetailsService.java
@@ -0,0 +1,38 @@
+package com.service;
+
+import com.User.User;
+import com.User.UserDAO;
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.*;
+
+@Service
+@AllArgsConstructor
+public class UserDetailsService implements org.springframework.security.core.userdetails.UserDetailsService {
+
+    private final UserDAO userDAO;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+
+        boolean accountNonExpired = true;
+        boolean credentialsNonExpired = true;
+        boolean accountNonLocked = true;
+
+        User user = userDAO.findUserByEmail(email);
+        return new org.springframework.security.core.userdetails.User(user.getEmail(),
+                user.getPassword(), user.isEnabled(), accountNonExpired, credentialsNonExpired, accountNonLocked,
+                getAuthorities(user));
+    }
+
+    private List<GrantedAuthority> getAuthorities(User user) {
+        List<GrantedAuthority> authorities = new ArrayList<GrantedAuthority>();
+        return authorities;
+    }
+
+
+}
