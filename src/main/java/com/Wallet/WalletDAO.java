7829605146e472c84a91a77ed499c846599957b4
Cezary Chojnowski <chojnowski.czarek@gmail.com>
2019-09-10 17:02:28 +0200

    Changing packages

---
+++ src/main/java/com/Wallet/WalletDAO.java
@@ -0,0 +1,32 @@
+package com.Wallet;
+
+import com.User.User;
+import com.User.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class WalletDAO {
+
+    private final WalletRepository walletRepository;
+    private final UserRepository userRepository;
+
+    public List<Wallet> findWalletsByUser(User user){
+        return walletRepository.findWalletsByUser(user);
+    }
+
+    public Wallet createNewWallet(String nameWallet, float balance, String emailUser){
+        Wallet wallet = new Wallet.Builder()
+                .nameWallet(nameWallet)
+                .balance(balance)
+                .user(userRepository.findUserByEmail(emailUser).get())
+                .build();
+        return walletRepository.save(wallet);
+    }
+    public boolean checkIfUserHasWalletWithTheGivenName(List<Wallet> userWallets, String newWalletName){
+        return userWallets.stream().anyMatch(o -> o.getNameWallet().equals(newWalletName));
+    }
+}
