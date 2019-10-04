269e88655eb44249892cae7b812e1a31eb47e699
Cezary Chojnowski <chojnowski.czarek@gmail.com>
2019-10-04 10:26:28 +0200

    Creating method findCategoriesByUser in CategoryRepository

--- src/main/java/com/Category/CategoryRepository.java
+++ src/main/java/com/Category/CategoryRepository.java
@@ -1,10 +1,13 @@
 package com.Category;
 
+import com.User.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, Integer> {
-
+    List<Category> findCategoriesByUser(User user);
 }
