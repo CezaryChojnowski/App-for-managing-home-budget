c38770fac301e59d9fdf47b02afba13b79ba7b87
Cezary Chojnowski <chojnowski.czarek@gmail.com>
2019-10-14 15:03:26 +0200

    Implementation of method findAllTransactionByUser

--- src/main/java/com/homeBudget/domain/Transaction/TransactionDAO.java
+++ src/main/java/com/homeBudget/domain/Transaction/TransactionDAO.java
@@ -7,5 +7,17 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class TransactionDAO {
+    public final SubcategoryRepository subcategoryRepository;
+    public final TransactionRepository transactionRepository;
+    public final SubcategoryDAO subcategoryDAO;
 
+    public List<Transaction> findAllTransactionByUser(User user){
+        List<Subcategory> subcategories = subcategoryDAO.getAllSubcategoryByUser(user);
+        List<Transaction> transactions = new ArrayList<>();
+        for (Subcategory s: subcategories){
+            transactions.addAll(transactionRepository.findAllBySubcategory(s));
+        }
+        return transactions;
+
+    }
 }
--- src/main/java/com/homeBudget/domain/Transaction/TransactionRepository.java
+++ src/main/java/com/homeBudget/domain/Transaction/TransactionRepository.java
@@ -1,9 +1,14 @@
 package com.homeBudget.domain.Transaction;
 
+import com.homeBudget.domain.Subcategory.Subcategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.Date;
+import java.util.List;
 
 
 @Repository
 public interface TransactionRepository extends JpaRepository<Transaction, Integer> {
+ List<Transaction> findAllBySubcategory(Subcategory subcategory);
  }
