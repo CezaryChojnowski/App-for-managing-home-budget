fe58b2fb76b1542ca061fdc114eb801d973c32c1
Cezary Chojnowski <chojnowski.czarek@gmail.com>
2019-10-15 12:13:55 +0200

    Creating POST method that add transaction to database. User must choose wallet and subcategory. Event and Person isn't required

--- src/main/java/com/homeBudget/rest/SubcategoryRestController.java
+++ src/main/java/com/homeBudget/rest/SubcategoryRestController.java
@@ -1,12 +1,16 @@
 package com.homeBudget.rest;
 
 import com.homeBudget.domain.subcategory.SubcategoryDAO;
+import com.homeBudget.domain.transaction.Transaction;
+import com.homeBudget.domain.transaction.TransactionDAO;
 import com.homeBudget.domain.user.User;
 import com.homeBudget.domain.user.UserDAO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 
 @RestController
@@ -16,12 +20,21 @@ public class SubcategoryRestController {
 
     private final UserDAO userDAO;
     private final SubcategoryDAO subcategoryDAO;
+    private final TransactionDAO transactionDAO;
 
     @GetMapping
     public ResponseEntity getAllSubcategoryByUser(){
         String email = SecurityContextHolder.getContext().getAuthentication().getName();
         User user = userDAO.findUserByEmail(email);
         return ResponseEntity.ok(subcategoryDAO.getAllSubcategoryByUser(user));
+    }
+    @PostMapping("{idSubcategory}/transactions")
+    public ResponseEntity createNewTransaction(@PathVariable("idSubcategory") int idSubcategory,
+                                               @RequestParam(value = "idWallet") int idWallet,
+                                               @RequestParam(value = "idEvent", required = false) Long idEvent,
+                                               @RequestParam(value = "idPerson", required = false) Long idPerson,
+                                               @Valid @RequestBody Transaction transaction){
+        return ResponseEntity.ok(transactionDAO.createNewTransaction(transaction.getAmount(),transaction.getComment(),transaction.getDateTransaction(), idSubcategory, idWallet, idEvent, idPerson));
     }
 
 }
