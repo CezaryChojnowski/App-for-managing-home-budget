9fd68acfba0ad60dda282f04bafbe482f00dc7fd
Cezary Chojnowski <chojnowski.czarek@gmail.com>
2019-10-15 12:12:01 +0200

    Creating method that saves the transaction in the database

--- src/main/java/com/homeBudget/domain/transaction/TransactionDAO.java
+++ src/main/java/com/homeBudget/domain/transaction/TransactionDAO.java
@@ -1,12 +1,16 @@
 package com.homeBudget.domain.transaction;
 
+import com.homeBudget.domain.event.EventRepository;
+import com.homeBudget.domain.person.PersonRepository;
 import com.homeBudget.domain.subcategory.Subcategory;
 import com.homeBudget.domain.subcategory.SubcategoryDAO;
 import com.homeBudget.domain.subcategory.SubcategoryRepository;
 import com.homeBudget.domain.user.User;
+import com.homeBudget.domain.wallet.WalletRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -16,6 +20,9 @@ import java.util.List;
 public class TransactionDAO {
     public final SubcategoryRepository subcategoryRepository;
     public final TransactionRepository transactionRepository;
+    public final WalletRepository walletRepository;
+    public final EventRepository eventRepository;
+    public final PersonRepository personRepository;
     public final SubcategoryDAO subcategoryDAO;
 
     public List<Transaction> findAllTransactionByUser(User user){
@@ -25,6 +32,19 @@ public class TransactionDAO {
             transactions.addAll(transactionRepository.findAllBySubcategory(s));
         }
         return transactions;
-
     }
+
+    public Transaction createNewTransaction(float amount, String comment, LocalDate dateTransaction, int id_subcategory, int id_wallet, Long id_event, Long id_person){
+        Transaction transaction = new Transaction.Builder()
+                .amount(amount)
+                .comment(comment)
+                .dateTransaction(dateTransaction)
+                .subcategory(subcategoryRepository.findSubcategoryByIdSubcategory(id_subcategory))
+                .wallet(walletRepository.findWalletByIdWallet(id_wallet))
+                .event(eventRepository.findEventById(id_event))
+                .person(personRepository.findPersonById(id_person))
+                .build();
+        return transactionRepository.save(transaction);
+    }
+
 }
