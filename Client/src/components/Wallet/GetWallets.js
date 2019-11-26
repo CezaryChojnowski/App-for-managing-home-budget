69d20e73dd1af4a05251469086efb4c6c8ca35b3
Cezary Chojnowski <chojnowski.czarek@gmail.com>
2019-11-26 18:03:18 +0100

    Creating GetWallets component, that displays all wallets of the logged user

---
+++ Client/src/components/Wallet/GetWallets.js
@@ -0,0 +1,49 @@
+import React, { Component } from "react";
+import WalletItem from "./WalletItem";
+import CreateWalletItem from "./CreateWalletButton";
+import { connect } from "react-redux";
+import { getWallets } from "../../actions/walletActions";
+import PropTypes from "prop-types";
+
+class GetWallets extends Component {
+  componentDidMount() {
+    this.props.getWallets();
+  }
+
+  render() {
+    const {wallets} = this.props.wallet;
+    return (
+      <div className="wallets">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12">
+              <h1 className="display-4 text-center">Wallets</h1>
+              <br />
+              <CreateWalletItem />
+
+              <br />
+              <hr />
+              {wallets.map(wallet => (
+                <WalletItem key={wallet.id} wallet={wallet} />
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+GetWallets.propTypes = {
+  wallet: PropTypes.object.isRequired,
+  getWallets: PropTypes.func.isRequired
+};
+
+const mapStateToProps = state => ({
+  wallet: state.wallet
+});
+
+export default connect(
+  mapStateToProps,
+  { getWallets }
+)(GetWallets);
