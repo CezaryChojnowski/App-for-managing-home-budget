bf6f93cd393de70a8bc705ed94456ea437fb1afb
Cezary Chojnowski <chojnowski.czarek@gmail.com>
2019-12-03 12:14:00 +0100

    Creating action to managing post and creating component to add new category

--- Client/src/actions/subcategoryActions.js
+++ Client/src/actions/subcategoryActions.js
@@ -1,7 +1,16 @@
 import axios from "axios";
-import {GET_SUBCATEGORIES} from "./types";
+import {GET_SUBCATEGORIES, GET_ERRORS} from "./types";
 
 export const getSubcategories = () => async dispatch => {
     const res = await axios.get("http://localhost:8080/subcategories/all");
     dispatch({type: GET_SUBCATEGORIES, payload: res.data});
+};
+
+export const createSubcategory = (id, subcategory, history) => async dispatch => {
+    try {
+        const res = await axios.post(`http://localhost:8080/categories/${id}/subcategories`, subcategory);
+        history.push("/dashboard");
+    } catch (err) {
+        dispatch({type: GET_ERRORS, payload: err.response.data});
+    }
 };
