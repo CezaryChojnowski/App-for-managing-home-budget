721492bf15280d941ceda986b1fbf33bf6c405e2
Cezary Chojnowski <chojnowski.czarek@gmail.com>
2019-12-04 10:50:18 +0100

    Adding @CrossOrigin annotation to person controller

--- Server/src/main/java/com/homeBudget/rest/controller/PersonRestController.java
+++ Server/src/main/java/com/homeBudget/rest/controller/PersonRestController.java
@@ -10,18 +10,20 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.security.Principal;
 
 @RestController
 @RequestMapping("/persons")
 @RequiredArgsConstructor
+@CrossOrigin
 public class PersonRestController {
 
     public final PersonService personService;
     public final UserService userService;
 
     @GetMapping
-    public ResponseEntity getPersonsByUser(){
-        String email = SecurityContextHolder.getContext().getAuthentication().getName();
+    public ResponseEntity getPersonsByUser(Principal principal){
+        String email = userService.findUserByEmail(principal.getName()).getEmail();
         User user = userService.findUserByEmail(email);
         return ResponseEntity.ok(personService.getPersons(user));
     }
